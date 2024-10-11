75a9c43b55029f951e207a535f7612d7bed9dc0e
XCode-tech <xcode.tech101@gmail.com>
2024-10-11 13:21:05 +0530

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = 'https://test.trainomart.com/api'; // Replace with your backend API URL
+const API_URL = 'https://ec2-43-204-214-252.ap-south-1.compute.amazonaws.com/api'; // Replace with your backend API URL
 
 export default API_URL;
