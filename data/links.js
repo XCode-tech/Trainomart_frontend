5b5bda81ee82224905130982bef4fa5731effe2b
XCode-tech <xcode.tech101@gmail.com>
2024-10-14 14:55:42 +0530

    Update links.js

--- data/links.js
+++ data/links.js
@@ -1,5 +1,5 @@
 export const links = [
-  { id: 1, href: "/help-center", label: "Help" },
+  // { id: 1, href: "/help-center", label: "Help" },
   // { id: 2, href: "/terms", label: "Privacy Policy" },
   // { id: 3, href: "/terms", label: "Cookie Notice" },
   // { id: 4, href: "/terms", label: "Security" },
