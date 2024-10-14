74b6eec3de292e84028c3775079cdd3b60233c4d
XCode-tech <xcode.tech101@gmail.com>
2024-10-14 16:46:44 +0530

    Update brands.js

--- data/brands.js
+++ data/brands.js
@@ -1,8 +1,12 @@
 export const brands = [
-  "/assets/img/clients/1.svg",
-  "/assets/img/clients/2.svg",
-  "/assets/img/clients/3.svg",
-  "/assets/img/clients/4.svg",
-  "/assets/img/clients/5.svg",
-  "/assets/img/clients/6.svg",
+  // "/assets/img/clients/1.svg",
+  // "/assets/img/clients/2.svg",
+  // "/assets/img/clients/3.svg",
+  // "/assets/img/clients/4.svg",
+  // "/assets/img/clients/5.svg",
+  // "/assets/img/clients/6.svg",
+  "/assets/img/clients/american-express.png",
+  "/assets/img/clients/Tg-logo.png",
+  "/assets/img/clients/atos.png",
+  "/assets/img/clients/AT.png",
 ];
