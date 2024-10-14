7855fa1aa544d717c45b22f20c143f7df6847a60
XCode-tech <xcode.tech101@gmail.com>
2024-10-14 15:00:24 +0530

    Update socialLinks.js

--- data/socialLinks.js
+++ data/socialLinks.js
@@ -1,8 +1,8 @@
 export const socialMediaLinks = [
-  { id: 1, href: "#", iconClassName: "icon-facebook" },
-  { id: 2, href: "#", iconClassName: "icon-twitter" },
-  { id: 3, href: "#", iconClassName: "icon-instagram" },
-  { id: 4, href: "#", iconClassName: "icon-linkedin" },
+  { id: 1, href: "https://www.facebook.com/profile.php?id=61566279315276", iconClassName: "icon-facebook" },
+  // { id: 2, href: "#", iconClassName: "icon-twitter" },
+  { id: 2, href: "https://www.instagram.com/trainomart/?utm_source=ig_web_button_share_sheet", iconClassName: "icon-instagram" },
+  { id: 3, href: "https://www.linkedin.com/company/104781952/admin/dashboard/", iconClassName: "icon-linkedin" },
 ];
 
 //   iconClassName are from font awesome
