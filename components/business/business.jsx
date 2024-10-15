5b9f73ce1b9df1b22187d01150b57306860dd1c1
XCode-tech <xcode.tech101@gmail.com>
2024-10-15 16:06:50 +0530

    Update business.jsx

--- components/business/business.jsx
+++ components/business/business.jsx
@@ -15,7 +15,6 @@ export default function Business() {
 
                 <div>
                   <p className="page-header__text">
-                    Sub Title for business Page
                   </p>
                 </div>
               </div>
@@ -56,27 +55,27 @@ export default function Business() {
             </div>
 
             <div className="col-lg-5">
-              <h2 className="text-30 lh-16">
+              {/* <h2 className="text-30 lh-16">
                 Areas of Expertise
-              </h2>
+              </h2> */}
               <p className="text-dark-1 mt-30">
-                Whether you're an IT Professional
-                seeking Advanced Skill Development or
-                An Executive in need of Experienced Trainers,
-                Our Expertise is tailored to meet your needs with
-                Hands-On Practical Solutions.
+                We offer instructors for a wide range of courses that cover various topics including advanced
+                courses such as Open AI, Nvidia, Databricks, Copilot, Google Gemini and more. Our expert instructors are industry professionals with years of experience who bring real-world expertise to the classroom.
+                <br />
+                <br />
+                Our training programs are designed to be flexible and customizable to meet the specific needs of our clients. We offer both in-person and online training options, and can create customized training solutions for businesses of all sizes.
+
+
               </p>
               <br />
-              <p>Services we provide to the industry are:</p>
-              <ul className="list-disc list-inside pl-4"> {/* Tailwind CSS classes for styling the list */}
-                <li> - Contract Trainers</li>
-                <li> - Consulting IT Projects</li>
-                <li> - Instructor-led Courses</li>
-                <li> - Learning at Scale</li>
-              </ul>
+              <p>We, at Trainomart, are committed to providing the highest quality training experience possible. We believe that learning should be engaging and fun, and we strive to create a supportive and collaborative environment that encourages learners to achieve their goals.
+                <br />
+                <br />
+                We look forward to helping you succeed!</p>
+
               <div className="d-inline-block">
                 <Link
-                  href="/signup"
+                  href="/contact"
                   className="button -md -purple-1 text-white mt-30"
                 >
                   Contact Us Today!
