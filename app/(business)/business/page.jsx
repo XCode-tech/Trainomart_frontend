eed6dd4744a40eca35c6a0984e2dbf16b58df1e7
XCode-tech <xcode.tech101@gmail.com>
2024-10-15 12:07:41 +0530

    Add files via upload

---
+++ app/(business)/business/page.jsx
@@ -0,0 +1,56 @@
+
+
+
+
+
+import About from '@/components/about/About'
+import Business from '@/components/business/business'
+
+
+import Brands from '@/components/common/Brands'
+import Instructors from '@/components/common/Instructors'
+import PageLinks from '@/components/common/PageLinks'
+import Preloader from '@/components/common/Preloader'
+import TestimonialsOne from '@/components/common/TestimonialsOne'
+import WhyCourse from '@/components/homes/WhyCourse'
+import FooterFour from '@/components/layout/footers/FooterFour'
+
+
+import FooterOne from '@/components/layout/footers/FooterOne'
+import Header from '@/components/layout/headers/Header'
+import HeaderFour from '@/components/layout/headers/HeaderFour'
+import React from 'react'
+
+export const metadata = {
+  title: 'Business',
+  description:
+    '',
+  
+}
+
+export default function page() {
+  return (
+    <div className="main-content  ">
+      <Preloader/>
+
+        <HeaderFour/>
+        <div className="content-wrapper js-content-wrapper overflow-hidden">
+            <PageLinks/>
+            <Business/>
+            <WhyCourse/>
+            
+
+            <TestimonialsOne/>
+            {/* <Instructors/> */}
+            <Brands/>
+           
+
+            
+            
+            <FooterFour/>
+        </div>
+
+    </div>
+  )
+}
+
