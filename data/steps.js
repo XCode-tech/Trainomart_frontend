48dd12ea4440a0b2d4f355e9e25af951667a4f1d
XCode-tech <xcode.tech101@gmail.com>
2024-10-17 20:09:27 +0530

    Update steps.js

--- data/steps.js
+++ data/steps.js
@@ -38,6 +38,45 @@ export const steps = [
   },
 ];
 
+export const steps2 = [
+  {
+    id: 1,
+    icon: "",
+    title: "",
+    text: "",
+  },
+  {
+    id: 2,
+    icon: "icon-working-at-home-2 text-64 text-green-1",
+    title: "Instructor Led courses for Individuals",
+    text: "Our courses emphasize practical, real-world applications, ensuring that learners not only gain knowledge but also the hands-on experience needed to apply it in real-life scenarios. From AI to machine learning, we bridge the gap between theory and practice.",
+  },
+  {
+    id: 3,
+    icon: "icon-online-learning-4 text-64 text-green-1",
+    title: "Instructor Augmentation Services",
+    text: "Enhance your team’s training capabilities with our Instructor Augmentation Services. We provide skilled instructors to complement your in-house training programs, ensuring top-tier expertise and customized guidance for your workforce.",
+  },
+  {
+    id: 4,
+    icon: "icon-working-at-home-2 text-64 text-green-1",
+    title: "Specialized Courses for businesses",
+    text: "Trainomart delivers industry-specific, specialized courses to help businesses stay competitive. Our programs focus on key areas like AI, deep learning, machine learning, and cloud computing, tailored to address your organization’s unique needs and objectives.",
+  },
+  {
+    id: 5,
+    icon: "icon-graduation-1 text-64 text-green-1",
+    title: "Customized Courses Development",
+    text: "Expand your course portfolio with Trainomart’s white-label and co-branded solutions. Educational institutions and training providers can integrate our high-quality technical content into their platforms, customized to meet the needs of their learners.",
+  },
+  {
+    id: 6,
+    icon: "",
+    title: "",
+    text: "",
+  },
+];
+
 export const stepsTwo = [
   {
     id: 1,
