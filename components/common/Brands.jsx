42b4d89b24ce92a2d12ad0dfde20e7af4aa66070
XCode-tech <xcode.tech101@gmail.com>
2024-10-17 23:47:47 +0530

    Update Brands.jsx

--- components/common/Brands.jsx
+++ components/common/Brands.jsx
@@ -1,48 +1,62 @@
-"use client";
-import Image from "next/image";
-import { brands } from "../../data/brands";
+"use client"; 
+import Image from "next/image"; 
+import { brands } from "../../data/brands"; 
+import { Swiper, SwiperSlide } from "swiper/react"; 
+import { Navigation, Pagination, Autoplay } from "swiper"; 
+import "swiper/swiper-bundle.min.css"; // Import Swiper styles
+
 const Brands = ({ backgroundColorComponent, brandsTwo }) => {
   return (
-    <>
-      <section
-        className={` ${
-          brandsTwo ? "layout-pt-md" : "layout-pt-lg"
-        }  layout-pb-md  ${
-          backgroundColorComponent ? backgroundColorComponent : ""
-        } `}
-      >
-        <div className="container">
-          <div className="row justify-center">
-            <div className="col text-center">
-              <p className="text-lg text-dark-1">Trusted by the world’s best</p>
-            </div>
-          </div>
-          <div
-            className={`row y-gap-30 justify-between sm:justify-start items-center pt-60 md:pt-50`}
-          >
-            {brands.map((logo, i) => (
-              <div
-                data-aos="fade-up"
-                data-aos-duration="300"
-                key={i}
-                className="col-lg-auto col-md-3 col-sm-4 col-6"
-              >
-                <div className="d-flex justify-center items-center px-4">
-                  <Image
-                    className="w-1/1"
-                    src={logo && logo}
-                    alt="clients image"
-                    width={140}
-                    height={90}
-                    style={{ objectFit: "contain" }}
-                  />
-                </div>
-              </div>
-            ))}
+    <section
+      className={`${
+        brandsTwo ? "layout-pt-md" : "layout-pt-lg"
+      } layout-pb-md ${backgroundColorComponent ? backgroundColorComponent : ""}`}
+    >
+      <div className="container">
+        <div className="row justify-center">
+          <div className="col text-center">
+            <p className="text-lg text-dark-1">Trusted by the world’s best</p>
           </div>
         </div>
-      </section>
-    </>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          navigation
+          pagination={{ clickable: true }}
+          spaceBetween={30}
+          slidesPerView={1} // Adjust as needed
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+          autoplay={{
+            delay: 2000, // Adjust the delay between slides (in milliseconds)
+            disableOnInteraction: false, // Autoplay will not be disabled after user interactions
+          }}
+        >
+          {brands.map((logo, i) => (
+            <SwiperSlide key={i}>
+              <div className="d-flex justify-center items-center px-4">
+                <Image
+                  className="w-1/1"
+                  src={logo && logo}
+                  alt="clients image"
+                  width={140}
+                  height={90}
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
   );
 };
 
