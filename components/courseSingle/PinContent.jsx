b7f7b13dd16c0e99edb7e85a52629139cd9bec2d
XCode-tech <xcode.tech101@gmail.com>
2024-10-18 12:54:43 +0530

    Update PinContent.jsx

--- components/courseSingle/PinContent.jsx
+++ components/courseSingle/PinContent.jsx
@@ -175,7 +175,7 @@ export default function PinContent({ pageItem }) {
                 className="button -md -outline-dark-1 text-dark-1 w-1/1 mt-10"
                 disabled={isLoading} // Disable button when loading
               >
-                {isLoading ? "Submitting..." : "Enroll Now"}
+                <b>{isLoading ? "Submitting..." : "Enroll Now"}</b>
               </button>
             </form>
 
