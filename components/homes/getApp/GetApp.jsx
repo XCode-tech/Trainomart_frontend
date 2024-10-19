1097d4d468be7e94d00a467a24a074a71d5874b8
XCode-tech <xcode.tech101@gmail.com>
2024-10-19 08:59:26 +0530

    Update GetApp.jsx

--- components/homes/getApp/GetApp.jsx
+++ components/homes/getApp/GetApp.jsx
@@ -43,10 +43,10 @@ export default function GetApp() {
               <div className="col-auto">
                 <div className="footer-header-socials">
                   <div className="footer-header-socials__title text-dark-1">
-                    <b>Follow us on social media</b> <Socials />
+                    <b>Follow us on social media</b>
                   </div>
                   <div className="footer-header-socials__list">
-{/*                     <Socials /> */}
+                    <Socials />
                   </div>
                 </div>
               </div>
