1035c334105735026869491fb64505029e85e1d7
XCode-tech <xcode.tech101@gmail.com>
2024-10-20 04:35:40 +0530

    Update page.jsx

--- app/(coursesList)/courses-list-4/page.jsx
+++ app/(coursesList)/courses-list-4/page.jsx
@@ -2,16 +2,16 @@
 
 import PageLinks from '@/components/common/PageLinks'
 import Preloader from '@/components/common/Preloader'
-import CourseListFour from '@/components/courseList/CourseListFour'
-
-
+import CourseListFive from '@/components/courseList/CourseListFive'
+import FooterFour from '@/components/layout/footers/FooterFour'
 
 import FooterOne from '@/components/layout/footers/FooterOne'
 import Header from '@/components/layout/headers/Header'
+import HeaderFour from '@/components/layout/headers/HeaderFour'
 import React from 'react'
 
 export const metadata = {
-  title: 'Couese-list-4',
+  title: 'All Coueses',
   description:
     'Elevate your e-learning content with Educrat, the most impressive LMS template for online courses, education and LMS platforms.',
   
@@ -21,11 +21,11 @@ export default function page() {
   return (
     <div className="main-content  ">
       <Preloader/>
-        <Header/>
+        <HeaderFour/>
         <div className="content-wrapper  js-content-wrapper overflow-hidden">
             <PageLinks/>
-            <CourseListFour/>
-            <FooterOne/>
+            <CourseListFive/>
+            <FooterFour/>
         </div>
     </div>
   )
