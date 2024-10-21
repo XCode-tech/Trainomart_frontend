bd0ba45a3808c3d4ae47bfe34c3389b9dd9e44b1
XCode-tech <xcode.tech101@gmail.com>
2024-10-21 11:40:09 +0530

    Update CourseListFive.jsx

--- components/courseList/CourseListFive.jsx
+++ components/courseList/CourseListFive.jsx
@@ -184,12 +184,12 @@ useEffect(() => {
             <div className="row">
               <div className="col-auto">
                 <div>
-                  <h1 className="page-header__title">User Interface Courses</h1>
+                  <h1 className="page-header__title">Our Courses Portfolio</h1>
                 </div>
 
                 <div>
                   <p className="page-header__text">
-                    Write an introductory description of the category.
+                    
                   </p>
                 </div>
               </div>
