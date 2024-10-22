543deb870ac20ef5423b733cdcb6aba76284ad18
XCode-tech <xcode.tech101@gmail.com>
2024-10-22 17:36:29 +0530

    Update CourseListFour.jsx

--- components/courseList/CourseListFour.jsx
+++ components/courseList/CourseListFour.jsx
@@ -88,7 +88,7 @@ export default function CourseListFour() {
 
                     <div className="h-100 pt-15">
                       <div className="text-17 lh-15 fw-500 text-dark-1 mt-10">
-                        <Link href={/courses/${course.id}}>
+                        <Link href={`/courses/${course.id}`}>
                           {course.course_name}
                         </Link>
                       </div>
