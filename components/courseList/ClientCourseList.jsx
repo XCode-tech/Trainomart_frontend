da05bd013599426dee7e01259d5aeabd50902a3c
XCode-tech <xcode.tech101@gmail.com>
2024-10-25 01:47:13 +0530

    Update ClientCourseList.jsx

--- components/courseList/ClientCourseList.jsx
+++ components/courseList/ClientCourseList.jsx
@@ -1,22 +1,22 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation'; // Only used on the client
+import { useSearchParams } from 'next/navigation';
 import CourseListFour from '@/components/courseList/CourseListFour';
 
 export default function ClientCourseList() {
   const [searchTerm, setSearchTerm] = useState('');
   const searchParams = useSearchParams();
 
-  // Fetch the search query on client-side rendering
   useEffect(() => {
-    const searchQuery = searchParams.get('search') || ''; // Extract 'search' query param
-    setSearchTerm(searchQuery);
+    if (searchParams) {
+      const query = searchParams.get('search') || '';
+      setSearchTerm(query);
+    }
   }, [searchParams]);
 
   return (
     <div>
-      {/* Pass searchTerm to CourseListFour */}
       <CourseListFour tags={searchTerm} />
     </div>
   );
