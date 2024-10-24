ed1bebd75e5bbb1d8f3a57ffa95538774e28b779
XCode-tech <xcode.tech101@gmail.com>
2024-10-24 12:53:28 +0530

    Update CourseListFive.jsx

--- components/courseList/CourseListFive.jsx
+++ components/courseList/CourseListFive.jsx
@@ -763,41 +763,10 @@ useEffect(() => {
                         />
                         <div className="coursesCard__image_overlay rounded-top-8"></div>
                       </div>
-                      <div className="d-flex justify-between py-10 px-10 absolute-full-center z-3">
-                        {elm.popular && (
-                          <>
-                            <div>
-                              <div className="px-15 rounded-200 bg-purple-1">
-                                <span className="text-11 lh-1 uppercase fw-500 text-white">
-                                  Popular
-                                </span>
-                              </div>
-                            </div>
 
-                            <div>
-                              <div className="px-15 rounded-200 bg-green-1">
-                                <span className="text-11 lh-1 uppercase fw-500 text-dark-1">
-                                  Best sellers
-                                </span>
-                              </div>
-                            </div>
-                          </>
-                        )}
-                      </div>
                     </div>
 
                     <div className="h-100 pt-20 pb-15 px-30">
-                      <div className="d-flex items-center">
-                        <div className="text-14 lh-1 text-yellow-1 mr-10">
-                          {elm.rating}
-                        </div>
-                        <div className="d-flex x-gap-5 items-center">
-                          <Star star={elm.rating} />
-                        </div>
-                        <div className="text-13 lh-1 ml-10">
-                          ({elm.ratingCount})
-                        </div>
-                      </div>
 
                       <div className="text-17 lh-15 fw-500 text-dark-1 mt-10">
                         <Link className="linkCustom" href={`/courses/${elm.id}`}>
@@ -816,7 +785,7 @@ useEffect(() => {
                             />
                           </div>
                           <div className="text-14 lh-1">
-                            {elm.lesson} lesson
+                            {elm.lessons} lesson
                           </div>
                         </div>
 
