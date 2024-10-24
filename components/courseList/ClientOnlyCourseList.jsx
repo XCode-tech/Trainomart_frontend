074102bfc3c6195d0901ff9414746e1ed1f56a80
XCode-tech <xcode.tech101@gmail.com>
2024-10-25 01:55:04 +0530

    Update and rename ClientCourseList.jsx to ClientOnlyCourseList.jsx

--- components/courseList/ClientOnlyCourseList.jsx
+++ components/courseList/ClientOnlyCourseList.jsx
@@ -4,19 +4,20 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import CourseListFour from '@/components/courseList/CourseListFour';
 
-export default function ClientCourseList() {
+export default function ClientOnlyCourseList() {
   const [searchTerm, setSearchTerm] = useState('');
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams(); // Use searchParams only on the client
 
   useEffect(() => {
     if (searchParams) {
-      const query = searchParams.get('search') || '';
-      setSearchTerm(query);
+      const query = searchParams.get('search') || ''; // Get search term from query params
+      setSearchTerm(query); // Set the search term
     }
   }, [searchParams]);
 
   return (
     <div>
+      {/* Pass searchTerm as a prop to CourseListFour */}
       <CourseListFour tags={searchTerm} />
     </div>
   );
