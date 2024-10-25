16e64f8d292167b55b4e0fbe192f546e21b7112b
XCode-tech <xcode.tech101@gmail.com>
2024-10-25 16:10:05 +0530

    Update page.jsx

--- app/(coursesList)/courses-list/page.jsx
+++ app/(coursesList)/courses-list/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import PageLinks from '@/components/common/PageLinks';
 import Preloader from '@/components/common/Preloader';
@@ -8,12 +8,11 @@ import CourseListFour from '@/components/courseList/CourseListFour';
 import FooterFour from '@/components/layout/footers/FooterFour';
 import HeaderFour from '@/components/layout/headers/HeaderFour';
 
-export default function Page() {
+function CoursesContent() {
   const searchParams = useSearchParams();
   const [searchTerm, setsearchTerm] = useState('');
 
   useEffect(() => {
-    // Retrieve search term from URL query parameters
     const searchQuery = searchParams.get('searchTerm'); 
 
     if (searchQuery && searchQuery.length > 0) {
@@ -26,15 +25,22 @@ export default function Page() {
   console.log("searchTerm on page.jsx:", searchTerm); // Log the search term
 
   return (
+    <div className="content-wrapper js-content-wrapper overflow-hidden">
+      <PageLinks />
+      <CourseListFour tags={searchTerm} />
+      <FooterFour />
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
     <div className="main-content">
       <Preloader />
       <HeaderFour />
-      <div className="content-wrapper js-content-wrapper overflow-hidden">
-        <PageLinks />
-        {/* Ensure search is passed even if it's an empty string */}
-        <CourseListFour tags={searchTerm} />
-        <FooterFour />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CoursesContent />
+      </Suspense>
     </div>
   );
 }
