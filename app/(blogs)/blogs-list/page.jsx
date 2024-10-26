6c45cca252d40eb288285b71b7f2163c84e75419
XCode-tech <xcode.tech101@gmail.com>
2024-10-27 01:28:27 +0530

    Update page.jsx

--- app/(blogs)/blogs-list/page.jsx
+++ app/(blogs)/blogs-list/page.jsx
@@ -1,1 +1,45 @@
 
+
+
+
+
+
+
+
+import BlogsThree from '@/components/blogs/BlogsThree'
+import PageLinks from '@/components/common/PageLinks'
+import Preloader from '@/components/common/Preloader'
+import FooterFour from '@/components/layout/footers/FooterFour'
+
+import FooterOne from '@/components/layout/footers/FooterOne'
+import Header from '@/components/layout/headers/Header'
+import HeaderFour from '@/components/layout/headers/HeaderFour'
+import React from 'react'
+
+export const metadata = {
+  title: 'Blog-list',
+  description:
+    'Elevate your e-learning content with Educrat, the most impressive LMS template for online courses, education and LMS platforms.',
+  
+}
+
+export default function page() {
+  return (
+    <div className="main-content  ">
+      <Preloader/>
+
+        <HeaderFour/>
+        <div className="content-wrapper js-content-wrapper overflow-hidden">
+            <PageLinks/>
+
+            <BlogsThree/>
+           
+            
+            <FooterFour/>
+        </div>
+
+    </div>
+  )
+}
+
+
