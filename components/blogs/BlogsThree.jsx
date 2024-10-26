c65532d626a03267d7eecca665b203fc14efb0d6
XCode-tech <xcode.tech101@gmail.com>
2024-10-27 01:17:41 +0530

    Update BlogsThree.jsx

--- components/blogs/BlogsThree.jsx
+++ components/blogs/BlogsThree.jsx
@@ -1,9 +1,41 @@
-import React from "react";
-import { blogs } from "../../data/blog";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Pagination from "../common/Pagination";
 import Link from "next/link";
+import axios from "axios";
+
 export default function BlogsThree() {
+  const [blogs, setBlogs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const blogsPerPage = 4;
+
+  // Fetch blogs from the API on component mount
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get("https://test.trainomart.com/api/blogs/");
+        setBlogs(response.data); // Assuming response.data is an array of blogs
+        setTotalPages(Math.ceil(response.data.length / blogsPerPage));
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
+  // Calculate the current page's blogs
+  const indexOfLastBlog = currentPage * blogsPerPage;
+  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
+  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
+
+  // Handle pagination
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
       <section className="page-header -type-1">
@@ -14,7 +46,6 @@ export default function BlogsThree() {
                 <div>
                   <h1 className="page-header__title">Latest News</h1>
                 </div>
-
                 <div>
                   <p className="page-header__text">
                     We’re on a mission to deliver engaging, curated courses at a
@@ -30,8 +61,8 @@ export default function BlogsThree() {
       <section className="layout-pt-md layout-pb-lg">
         <div className="container">
           <div className="row y-gap-30 justify-center">
-            {blogs.slice(0, 4).map((elm, i) => (
-              <div key={i} className="col-lg-10 col-md-11">
+            {currentBlogs.map((elm) => (
+              <div key={elm.id} className="col-lg-10 col-md-11">
                 <div className="blogCard -type-3">
                   <div className="row y-gap-30 items-center">
                     <div className="col-lg-6">
@@ -40,27 +71,22 @@ export default function BlogsThree() {
                           width={510}
                           height={392}
                           className="rounded-8"
-                          src={elm.imageSrc}
+                          src={elm.blog_image}
                           alt="image"
                         />
                       </div>
                     </div>
-
                     <div className="col-lg-6">
                       <div className="blogCard__content pl-60 lg:pl-40 md:pl-0">
                         <div className="blogCard__category text-14 lh-1 text-purple-1 fw-500">
-                          EDUCATION
+                          {elm.category}
                         </div>
                         <h4 className="blogCard__title text-24 lh-15 text-dark-4 fw-500 mt-15">
-                          <Link
-                            className="linkCustom"
-                            href={`/blogs/${elm.id}`}
-                          >
-                            {" "}
-                            {elm.title}
+                          <Link className="linkCustom" href={`/blogs/${elm.id}`}>
+                            {elm.blog_title}
                           </Link>
                         </h4>
-                        <p className="blogCard__text mt-20">{elm.desc}</p>
+                        <p className="blogCard__text mt-20">{elm.blog_data.split("\r\n")[0]}</p>
                         <div className="blogCard__button d-inline-block mt-20">
                           <Link
                             href={`/blogs/${elm.id}`}
@@ -79,7 +105,11 @@ export default function BlogsThree() {
 
           <div className="row justify-center pt-60 lg:pt-40">
             <div className="col-auto">
-              <Pagination />
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+              />
             </div>
           </div>
         </div>
