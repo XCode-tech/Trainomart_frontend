d91ede77418c37a4d03b209463bcdf7849602fd1
XCode-tech <xcode.tech101@gmail.com>
2024-10-28 15:35:18 +0530

    Update CourseListFour.jsx

--- components/courseList/CourseListFour.jsx
+++ components/courseList/CourseListFour.jsx
@@ -115,16 +115,11 @@ export default function CourseListFour({ tags = "" }) {
 
                       <div className="coursesCard-footer">
                         <div className="coursesCard-footer__price">
-{/*                           {course.price ? (
-                            <>
-                              <div>${course.original_price}</div>
-                              <div>${course.price}</div>
-                            </>
-                          ) : (
-                            <div>Free</div>
-                          )} */}
-                          <div className="">{course.orignal_price}</div>
-                          <div className="">{course.price}</div>
+                        <div className="coursesCard-footer__price">
+                          <div className="">${elm.orignal_price}</div>
+                          <div className="">${elm.price}</div>
+                        </div>
+
                         </div>
                       </div>
                     </div>
