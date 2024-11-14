b8ac0b44b2883cd1d51e2fc0186f735175f6321d
XCode-tech <xcode.tech101@gmail.com>
2024-11-14 23:39:59 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,13 +32,7 @@ export default function RootLayout({ children }) {
 
         
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZQ8GQNM2P"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-BZQ8GQNM2P');
-        </script>
+
 
       </head>
       <body>
