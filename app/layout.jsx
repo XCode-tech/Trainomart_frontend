958c57429ac6e33bd885c73fe5285364fb448dcd
XCode-tech <xcode.tech101@gmail.com>
2024-11-14 23:27:29 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,15 +25,15 @@ export default function RootLayout({ children }) {
     <html lang="en" className="">
       <head>
         <meta name="google-site-verification" content="9RQxVTNRbHczszboYPDw4kQFTBBFHCrgPp5yOl3TSYQ" />
-        <!-- Google Tag Manager -->
+        
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-M2QVLW2N');</script>
-        <!-- End Google Tag Manager -->
+        
 
-        <!-- Google tag (gtag.js) -->
+        
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZQ8GQNM2P"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
@@ -45,10 +45,10 @@ export default function RootLayout({ children }) {
 
       </head>
       <body>
-        <!-- Google Tag Manager (noscript) -->
+        
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2QVLW2N"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+       
 
         <Context> {children}</Context>
       </body>
