c14dad1ce25753e8a64fc110d29e406af5cacf1b
XCode-tech <xcode.tech101@gmail.com>
2024-11-15 00:06:08 +0530

    Update Preloader.jsx

--- components/common/Preloader.jsx
+++ components/common/Preloader.jsx
@@ -10,11 +10,6 @@ export default function Preloader() {
 
   return (
     <div className="preloader js-preloader">
-      <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2QVLW2N"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
-
       <div
         className="preloader__bg"
         style={preloaderDisable ? { transform: "scale(1,0)" } : {}}
