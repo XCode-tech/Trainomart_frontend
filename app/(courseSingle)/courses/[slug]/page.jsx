5389042fb413e254d489ea269131623d3d92bbd2
XCode-tech <xcode.tech101@gmail.com>
2024-11-20 13:48:36 +0530

    Update page.jsx

--- app/(courseSingle)/courses/[slug]/page.jsx
+++ app/(courseSingle)/courses/[slug]/page.jsx
@@ -19,7 +19,7 @@ export default function Page({ params }) {
     const fetchMetadata = async () => {
       setIsLoading(true);
       try {
-        const res = await fetch(`https://test.trainomart.com/api/courses/slug/${params.slug}/`);
+        const res = await fetch(`https://test.trainomart.com/api/courses/${params.slug}/`);
         if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
         
         const data = await res.json();
