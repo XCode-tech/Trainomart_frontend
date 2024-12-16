6d3e32779ec97dfe629b4d01a9600d32b9d1b38b
XCode-tech <xcode.tech101@gmail.com>
2024-12-16 17:40:14 +0530

    Update HeroFour.jsx

--- components/homes/heros/HeroFour.jsx
+++ components/homes/heros/HeroFour.jsx
@@ -91,7 +91,7 @@ export default function HeroFour() {
       <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
         <div className="bg-white rounded-lg shadow-xl max-w-4xl w-full mx-4 overflow-hidden">
           {/* Flex Container */}
-          <div className="flex">
+          <div className="flex-row">
             {/* Left Side - Image */}
             <div className="w-1/2">
               <Image
