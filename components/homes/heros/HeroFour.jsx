75be33b1e987ceed843bb55f3d83d3005991ea62
XCode-tech <xcode.tech101@gmail.com>
2024-12-17 13:34:21 +0530

    Update HeroFour.jsx

--- components/homes/heros/HeroFour.jsx
+++ components/homes/heros/HeroFour.jsx
@@ -124,10 +124,16 @@ export default function HeroFour() {
                   className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
                 />
                 <br />
-
-                <button type="button" name="submit" id="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                  Default
+                <button
+                  type="submit"
+                  name="submit"
+                  id="submit"
+                  className="w-full py-3 rounded-lg bg-purple-600 text-black hover:bg-purple-700"
+                  style={{ backgroundColor: "blue", fontWeight: "bold" }}
+                >
+                  Sign Up
                 </button>
+
               </form>
               {/* Close Button */}
               <button
