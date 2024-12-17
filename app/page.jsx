a7d69945d042e94082ee2514ffacabf55bf743fc
XCode-tech <xcode.tech101@gmail.com>
2024-12-17 17:55:42 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -33,7 +33,27 @@ export default function HomePage() {
     <>
     <Preloader/>
     <HeaderFour />
-    
+    <div dangerouslySetInnerHTML={{
+      __html: `
+      <!-- Meta Pixel Code -->
+      <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '845551474356300');
+        fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=845551474356300&ev=PageView&noscript=1" />
+      </noscript>
+      <!-- End Meta Pixel Code -->
+    ` }} />
+
     <div className="content-wrapper  js-content-wrapper overflow-hidden">
     
       <HeroFour />
