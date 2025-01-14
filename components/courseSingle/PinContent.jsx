8fa417bbe4adff618da3f978ae42f6ecdb55aa4d
XCode-tech <xcode.tech101@gmail.com>
2025-01-14 13:42:25 +0530

    Update PinContent.jsx

--- components/courseSingle/PinContent.jsx
+++ components/courseSingle/PinContent.jsx
@@ -516,6 +516,7 @@ export default function PinContent({ pageItem }) {
                 onClick={() => handleBuyNow(pageItem.slug)}
               >
                 <b>Buy Now</b>
+              </button>
             </form>
 
            
