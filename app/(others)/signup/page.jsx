6609831f78c3b21badb4f01e1710fa614cffc598
XCode-tech <xcode.tech101@gmail.com>
2025-02-20 11:37:42 +0530

    Update page.jsx

--- app/(others)/signup/page.jsx
+++ app/(others)/signup/page.jsx
@@ -12,6 +12,7 @@ import Preloader from '@/components/common/Preloader'
 
 import FooterOne from '@/components/layout/footers/FooterOne'
 import Header from '@/components/layout/headers/Header'
+import HeaderFour from '@/components/layout/headers/HeaderFour'
 import HeaderAuth from '@/components/layout/headers/HeaderAuth'
 import AuthImageMove from '@/components/others/AuthImageMove'
 import LoginForm from '@/components/others/LoginForm'
@@ -22,23 +23,25 @@ export const metadata = {
   title: 'Sign up',
   description:
     'Elevate your e-learning content with Educrat, the most impressive LMS template for online courses, education and LMS platforms.',
-  
+
 }
 export default function page() {
   return (
     <div className="main-content  ">
-      <Preloader/>
+      <Preloader />
 
-        <HeaderAuth/>
-        <div className="content-wrapper js-content-wrapper overflow-hidden">
-            <section  className="form-page js-mouse-move-container">
-                <AuthImageMove/>
-                <SignUpForm/>
-            </section>
-           
-            
-            
-        </div>
+      {/* <HeaderAuth/> */}
+      <HeaderFour />
+
+      <div className="content-wrapper js-content-wrapper overflow-hidden">
+        <section className="form-page js-mouse-move-container">
+          <AuthImageMove />
+          <SignUpForm />
+        </section>
+
+
+
+      </div>
 
     </div>
   )
