060f8bb2c4775c8114bf04138034e20e0f556344
XCode-tech <xcode.tech101@gmail.com>
2025-02-20 11:38:23 +0530

    Update AuthImageMove.jsx

--- components/others/AuthImageMove.jsx
+++ components/others/AuthImageMove.jsx
@@ -39,58 +39,17 @@ export default function AuthImageMove() {
     parallaxIt();
   }, []);
   return (
-    <div className="form-page__img bg-dark-1">
+    <div className="form-page__img bg-light-6">
       <div className="form-page-composition">
-        <div className="-bg">
+        <div className="masthead-image__img1">
+
           <Image
-            width={1250}
-            height={1000}
-            style={{ width: "100%" }}
-            data-move="30"
-            className="js-mouse-move"
-            src="/assets/img/login/bg.png"
-            alt="bg"
-          />
-        </div>
-        <div className="-el-1">
-          <Image
-            width={1250}
-            height={1000}
-            style={{ width: "100%" }}
+            width={587}
+            height={656}
             data-move="20"
             className="js-mouse-move"
-            src="/assets/img/home-9/hero/bg.png"
-            alt="image"
-          />
-        </div>
-        <div className="-el-2">
-          <Image
-            width={488}
-            height={485}
-            data-move="40"
-            className="js-mouse-move"
-            src="/assets/img/home-9/hero/1.png"
-            alt="icon"
-          />
-        </div>
-        <div className="-el-3">
-          <Image
-            width={108}
-            height={108}
-            data-move="40"
-            className="js-mouse-move"
-            src="/assets/img/home-9/hero/2.png"
-            alt="icon"
-          />
-        </div>
-        <div className="-el-4">
-          <Image
-            width={108}
-            height={108}
-            data-move="40"
-            className="js-mouse-move"
-            src="/assets/img/home-9/hero/3.png"
-            alt="icon"
+            src="/assets/img/home-4/masthead/G-1.png"
+            alt="Hero Image"
           />
         </div>
       </div>
