05e5d5de239392b86e9b14f6f5b72340c430833e
XCode-tech <xcode.tech101@gmail.com>
2025-02-24 20:28:45 +0530

    Update HeroFour.jsx

--- components/homes/heros/HeroFour.jsx
+++ components/homes/heros/HeroFour.jsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation"; 
 import API_URL from "@/data/config";
 import { X } from 'lucide-react';
+import Head from "next/head";
 // import { Button } from "@/components/ui/button";
 // import { Input } from "@/components/ui/input";
 
