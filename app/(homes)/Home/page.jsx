81ea7d3b68b003939e4311285cc6d16281c0c2a1
XCode-tech <xcode.tech101@gmail.com>
2025-03-06 13:12:20 +0530

    Update page.jsx

--- app/(homes)/Home/page.jsx
+++ app/(homes)/Home/page.jsx
@@ -7,6 +7,7 @@ import React from 'react'
 import HeaderFour from '@/components/layout/headers/HeaderFour'
 import HeroFour from '@/components/homes/heros/HeroFour'
 import Brands from '@/components/common/Brands'
+import RootLayout from '@/app/layout'
 
 import CategoriesFour from '@/components/homes/categories/CategoriesFour'
 import CoursesFour from '@/components/homes/courses/CoursesFour'
@@ -20,39 +21,42 @@ import EventsFour from '@/components/homes/events/EventsFour'
 import LearningSelection from '@/components/homes/LearningSelection'
 import FooterFour from '@/components/layout/footers/FooterFour'
 import Preloader from '@/components/common/Preloader'
-export const metadata = {
-  title: 'Home',
-  description:
-    'Elevate your e-learning content with Educrat, the most impressive LMS template for online courses, education and LMS platforms.',
-  
-}
 
+
+export const metadata = {
+  title: 'Trainomart - Best Online Training & Certification Courses',
+  description:
+    'Enhance your skills with TrainoMart’s top-rated online training and certification courses. Learn from industry experts and boost your career today! Enroll Now.',
+  canonical: 'https://www.trainomart.com/',
+}
 
 
 
 export default function page() {
   return (
     <>
-    <div className='main-content'>
-    <Preloader/>
-        <HeaderFour/>
-    </div>
-     <div className="content-wrapper  js-content-wrapper overflow-hidden">
-        <HeroFour/>
-        <Brands/>
-        <CategoriesFour/>
-        <CoursesFour/>
-        <LearningPath/>
-        <FeaturedCourses/>
-        <LandJob/>
-        <TestimonialsFour/>
-        <AchievementsTwo/>
-        <InstractorFour/>
-        <EventsFour/>
-        <LearningSelection/>
-        <FooterFour/>
+      <RootLayout metadata={metadata}>
+        <div className='main-content'>
+          <Preloader />
+          <HeaderFour />
+        </div>
+        <div className="content-wrapper  js-content-wrapper overflow-hidden">
+          <HeroFour />
+          <Brands />
+          <CategoriesFour />
+          <CoursesFour />
+          <LearningPath />
+          <FeaturedCourses />
+          <LandJob />
+          <TestimonialsFour />
+          <AchievementsTwo />
+          <InstractorFour />
+          <EventsFour />
+          <LearningSelection />
+          <FooterFour />
 
-     </div>
-     </>
+        </div>
+      </RootLayout>
+    </>
   )
 }
