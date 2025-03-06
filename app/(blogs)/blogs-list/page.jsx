e5089bcc4ca82a3208d084252843f5363e2eee2b
XCode-tech <xcode.tech101@gmail.com>
2025-03-06 12:17:45 +0530

    Update page.jsx

--- app/(blogs)/blogs-list/page.jsx
+++ app/(blogs)/blogs-list/page.jsx
@@ -1,47 +1,28 @@
-
-
-
-
-
-
-
-
 import BlogsThree from '@/components/blogs/BlogsThree'
 import PageLinks from '@/components/common/PageLinks'
 import Preloader from '@/components/common/Preloader'
 import FooterFour from '@/components/layout/footers/FooterFour'
-import RootLayout from '@/app/layout'
-import FooterOne from '@/components/layout/footers/FooterOne'
-import Header from '@/components/layout/headers/Header'
 import HeaderFour from '@/components/layout/headers/HeaderFour'
-import React from 'react'
 
 export const metadata = {
   title: 'Trainomart Blogs - Latest Insights on Training & Certification',
   description:
     'Stay updated with Trainomart’s latest blogs on career growth, online training, certification courses, and industry trends. Explore expert insights now!',
-  Canonical_tag: 'https://www.trainomart.com/blogs-list/',
+  alternates: {
+    canonical: 'https://www.trainomart.com/blogs-list/',
+  },
 }
 
-export default function page() {
+export default function Page() {
   return (
-    <RootLayout metadata={metadata}>
-      <div className="main-content  ">
-        <Preloader />
-
-        <HeaderFour />
-        <div className="content-wrapper js-content-wrapper overflow-hidden">
-          <PageLinks />
-
-          <BlogsThree />
-
-
-          <FooterFour />
-        </div>
-
+    <div className="main-content">
+      <Preloader />
+      <HeaderFour />
+      <div className="content-wrapper js-content-wrapper overflow-hidden">
+        <PageLinks />
+        <BlogsThree />
+        <FooterFour />
       </div>
-    </RootLayout>
+    </div>
   )
 }
-
-
