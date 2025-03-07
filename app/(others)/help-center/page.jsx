ecdd7595c54d4e8be668c482421abc6acb43212b
XCode-tech <xcode.tech101@gmail.com>
2025-03-07 13:16:46 +0530

    Update page.jsx

--- app/(others)/help-center/page.jsx
+++ app/(others)/help-center/page.jsx
@@ -12,6 +12,7 @@
 import Faq from '@/components/common/Faq'
 import PageLinks from '@/components/common/PageLinks'
 import Preloader from '@/components/common/Preloader'
+import RootLayout from '@/app/layout'
 
 import FooterFour from '@/components/layout/footers/FooterFour'
 import HeaderFour from '@/components/layout/headers/HeaderFour'
@@ -22,22 +23,29 @@ export const metadata = {
   title: 'Help Center - Trainomart Support & FAQs',
   description:
     'Need assistance? Visit Trainomart’s Help Center for FAQs, course support, and technical guidance. Get quick solutions to all your queries!',
-  
+  canonical: 'https://www.trainomart.com/help-center',
 }
+
+
+
 
 export default function page() {
   return (
-    <div className="main-content  ">
-      <Preloader/>
+    <RootLayout metadata={metadata}>
 
-        <HeaderFour/>
+      <div className="main-content  ">
+        <Preloader />
+
+        <HeaderFour />
         <div className="content-wrapper js-content-wrapper overflow-hidden">
-            <PageLinks/>
-            <HelpCenter/>
-            <Faq/>
-            <FooterFour/>
+          <PageLinks />
+          <HelpCenter />
+          <Faq />
+          <FooterFour />
         </div>
 
-    </div>
+      </div>
+    </RootLayout>
+
   )
 }
